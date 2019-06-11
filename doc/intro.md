00493fe6d37d9caf747d1bf4ae8a2d005bbcda55
Lane Spangler <lspangler@protonmail.com>
2019-06-11 14:46:20 -0500

    Initialize with lein app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-webhook-consumer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_webhook_consumer/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-webhook-consumer.core-test
+  (:require [clojure.test :refer :all]
+            [clj-webhook-consumer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
