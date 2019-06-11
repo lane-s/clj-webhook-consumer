e2d11fffc12b880f962f6b2ac02b927760915c21
Lane Spangler <lspangler@protonmail.com>
2019-06-11 14:56:04 -0500

    Add project description and repo url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-webhook-consumer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An easily configurable server for consuming webhooks by running shell scripts"
+  :url "https://github.com/lane-s/clj-webhook-consumer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
