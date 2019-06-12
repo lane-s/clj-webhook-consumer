9f4a4ea516b6ecc3960322f4f67acec97366a570
Lane Spangler <lspangler@protonmail.com>
2019-06-11 22:16:09 -0500

    Execute scripts with environment variables set according to the config

--- src/clj_webhook_consumer/gen_app.clj
+++ src/clj_webhook_consumer/gen_app.clj
@@ -9,23 +9,59 @@
             [clojure.java.shell :refer [sh]])
   (:gen-class))
 
-(defn invalid-key? [local-key {request-key "key"}]
+(defn- invalid-key?
+  [local-key {request-key "key"}]
   (and local-key (not= local-key request-key)))
 
-(defn execute-scripts [path hook]
-  (doseq [script (:script hook)]
-    (sh (str path script))))
+(defn- get-env-kv-pairs
+  "Returns a sequence of key value pairs
+  where the keys are the leaves of `name-map`
+  and the values are the leaves of `value-map`"
+  [name-map value-map]
+  (reduce (fn [acc [nk nv]]
+            (let [v (get value-map nk)]
+              (if (map? nv)
+                (concat acc (get-env-kv-pairs nv v))
+                (let [env-name (if (= nv "auto")
+                                 (-> (str nk) (subs 1) clojure.string/upper-case)
+                                 nv)]
+                  (conj acc [env-name v]))))) '() name-map))
 
-(defn hook-to-route [key path hook]
+(defn- get-env
+  "Get an environment variable map
+  where keys are the names specified
+  in the config and values are grabbed
+  from the request body"
+  [hook body-params]
+  (->>
+   (get-env-kv-pairs (:env hook) body-params)
+   (into {})))
+
+(defn- execute-scripts
+  "Execute each script for `hook` with environment
+  variables specified in `env`"
+  [path hook env]
+  (doseq [script (:script hook)]
+    (sh (str path script) :env env)))
+
+(defn- hook->route
+  "Transform `hook` into an endpoint
+  that will execute the scripts associated
+  with `hook` when it is hit with a valid
+  `key`"
+  [key path hook]
   [(str "/" (:name hook))
    {:post (fn [{:keys [body-params query-params]}]
             (if (invalid-key? key query-params) {:status 401}
-              (do (execute-scripts path hook)
+                (do (execute-scripts path hook (get-env hook body-params))
                 {:status 200})))}])
 
-(defn gen-routes [path]
+(defn- gen-routes
+  "Generate endpoints from the config file.
+  `path` is prepended to all script paths"
+  [path]
   (let [config (yaml/from-file (str path "./.clj-webhook.yaml") true)]
-    (map (partial hook-to-route (:key config) path) (:hooks config))))
+    (map (partial hook->route (:key config) path) (:hooks config))))
 
 (defn gen-app
   ([path]
--- test/clj_webhook_consumer/gen_app_test.clj
+++ test/clj_webhook_consumer/gen_app_test.clj
@@ -7,13 +7,16 @@
 (def test-path "./test/clj_webhook_consumer/")
 (def out-file-1 (str test-path "test_out.txt"))
 (def out-file-2 (str test-path "test_out_2.txt"))
+(def test-env-file (str test-path "test_env_out.txt"))
 
 (defn out-file-fixture [f]
   (spit out-file-1 "")
   (spit out-file-2 "")
+  (spit test-env-file "")
   (f)
   (io/delete-file out-file-1)
-  (io/delete-file out-file-2))
+  (io/delete-file out-file-2)
+  (io/delete-file test-env-file))
 
 (use-fixtures :once out-file-fixture)
 
@@ -35,11 +38,16 @@
     (let [res (test-request "/testhookA?key=test_api_key")]
       (is (= (:status res) 200)))
     (is (= (slurp out-file-1) "Test\n")))
+
+  (testing "Request to testhookA runs env_test.sh with environment variables set"
+    (is (= (slurp test-env-file) "Some repo\nDocker file contents\n")))
+
   (testing "Request to testHookB runs test_2.sh"
     (is (= (slurp out-file-2) ""))
     (let [res (test-request "/testhookB?key=test_api_key")]
       (is (= (:status res) 200)))
     (is (= (slurp out-file-2) "Test 2\n")))
+
   (testing "Request to testHookB without key fails"
     (let [res (test-request "/testhookB")]
       (is (= (:status res) 401)))))
