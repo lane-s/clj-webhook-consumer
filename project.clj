7ff53da742d68424ef45d5bdd20e10b3582fe046
Lane Spangler <lspangler@protonmail.com>
2019-06-12 13:27:11 -0500

    Update to version 0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-webhook-consumer "0.1.0-SNAPSHOT"
+(defproject clj-webhook-consumer "0.2.0-SNAPSHOT"
   :description "An easily configurable server for consuming webhooks by running shell scripts"
   :url "https://github.com/lane-s/clj-webhook-consumer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
